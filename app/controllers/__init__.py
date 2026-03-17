3e60919c3d9ae9dc7a6565cbcbff334cbf20f8cb
AdrianDonairee <al.donaire@alumno.um.edu.ar>
2026-03-17 19:34:04 -0300

    Se prueban test de api key, url de api y motor de ia

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,4 +1,7 @@
 from .example_controller import example_bp
+from .ai_controller import ai_bp
+
 
 def register_blueprints(app):
-    app.register_blueprint(example_bp, url_prefix='/api/example')
+    app.register_blueprint(example_bp, url_prefix="/api/example")
+    app.register_blueprint(ai_bp, url_prefix="/api/ai")
